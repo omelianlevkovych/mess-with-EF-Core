1b83753cee4c35b5e84bc3dd59981f202f7e1112
omlbucket <omelianai@gmail.com>
2020-06-26 12:25:53 +0300

    Initial create. Added author and book entities with db context.

---
+++ 01-SimpleEFCore/src/SimpleEFCore/Author.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleEFCore
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        public string Name { get; set; }
+        public string WebUri { get; set; }
+    }
+}
---
+++ 01-SimpleEFCore/src/SimpleEFCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleEFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
